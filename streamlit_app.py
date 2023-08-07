7baef7cf295534c7260ad875e2eddf32d89a6bd1
AishaNoory <126344601+AishaNoory@users.noreply.github.com>
2023-08-07 10:35:07 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,6 +12,10 @@ if st.button("Get Summary"):
     if user_input:
         # Send a request to your model API to retrieve the summary
         model_api_url = " https://23b0-196-216-70-186.ngrok-free.app/summarize"  # Replace with your actual model API URL
+        response = requests.post(api_url, json=data)
+        print(response.status_code)
+        print(response.text)
+
         payload = {"input": user_input}
         
         try:
